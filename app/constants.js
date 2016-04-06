0147e8f0455cc048873c1195f3c9506f073d41cc
zui xiao <810795297@qq.com>
2016-04-06 19:55:46 +0800

    Create constants.js

---
+++ app/constants.js
@@ -0,0 +1,8 @@
+/**
+*创建不可变对象，该对象用来给每个动作定义一个独特名称。
+*/
+export default {
+  CREATED_ACCOUNT: 'created account',
+  WITHDREW_FROM_ACCOUNT: 'withdrew form account',
+  DEPOSITED_INTO_ACCOUNT: 'deposited into account'
+};
